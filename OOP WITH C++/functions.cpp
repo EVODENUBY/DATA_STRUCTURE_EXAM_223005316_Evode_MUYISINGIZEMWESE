0a34975eed3490b637d8e7e04d1b46ac9bf9a2f1
Evode MUYISINGIZEMWESE <evodepro6@gmail.com>
2025-02-20 16:57:20 +0200

    functions.cpp

--- OOP WITH C++/functions.cpp
+++ OOP WITH C++/functions.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int average(int a, int b){
+double average(int a, int b){
     return (a + b)/2;
 };
 int main(){
-    int result = average(43,176);
+    double result = average(43,176);
     cout<< "the average is : "<<result <<endl;
 
     return 0;
