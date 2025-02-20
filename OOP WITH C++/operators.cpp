4944feb55eeb4efca41eeb98dc39673db4a485fb
Evode MUYISINGIZEMWESE <evodepro6@gmail.com>
2025-02-20 15:27:39 +0200

    Add files via upload

---
+++ OOP WITH C++/operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 40, b = 17;
+    cout << "\nSum of a and b: " << a + b << endl;
+    cout << "Difference of a and b: " << a - b << endl;
+    cout << "Product of a and b: " << a * b << endl;
+    cout << "Quotient : " << a / b << endl;
+    cout << "Remainder: " << a % b << endl;
+    return 0;
+
+}
